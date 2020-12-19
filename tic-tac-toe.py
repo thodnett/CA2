08a5f67d51ec5deb226fe07549cb8183e8c16ab5
thodnett <taniahodnett@yahoo.ie>
2020-12-19 20:07:14 +0000

    Added comments and main game function.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,15 +1,21 @@
-
+"""Draws the board"""
 board = [' ' for x in range(10)]
 
 def insertLetter(letter, pos):
+    """Inserts the given letter in the given position.
+    """
     board[pos] = letter
 
 def spaceIsFree(pos):
+    """Checks if board space is free.
+    """
     return board[pos] == ' '
 
 def printBoard(board):
+    """Prints the board.
+    """
     print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('   |   |')
     print('-----------')
     print('   |   |')
@@ -17,13 +23,17 @@ def printBoard(board):
     print('   |   |')
     print('-----------')
     print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
 
 def isWinner(bo, le):
+    """Checks the board for a winner.
+    """
     return (bo[7] == le and bo[8] == le and bo[9] == le) or (bo[4] == le and bo[5] == le and bo[6] == le) or(bo[1] == le and bo[2] == le and bo[3] == le) or(bo[1] == le and bo[4] == le and bo[7] == le) or(bo[2] == le and bo[5] == le and bo[8] == le) or(bo[3] == le and bo[6] == le and bo[9] == le) or(bo[1] == le and bo[5] == le and bo[9] == le) or(bo[3] == le and bo[5] == le and bo[7] == le)
 
 def playerMove():
+    """Function which handles human players moves.
+    """
     run = True
     while run:
         move = input('Please select a position to place an \'X\' (1-9): ')
@@ -41,6 +51,8 @@ def playerMove():
             print('Please type a number!') 
 
 def compMove():
+    """Function that handles AI player moves.
+    """
     possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
     move = 0
 
@@ -76,6 +88,8 @@ def compMove():
     return move
 
 def selectRandom(li):
+    """Selects a random move from the possible moves.
+    """
     import random
     ln = len(li)
     r = random.randrange(0,ln)
@@ -83,7 +97,50 @@ def selectRandom(li):
     
 
 def isBoardFull(board):
+    """Checks if the board is full
+    """
     if board.count(' ') > 1:
         return False
     else:
         return True
+
+def main():
+    """Main function for gameplay.
+    """
+    print('Welcome to Tic Tac Toe!')
+    printBoard(board)
+
+    while not(isBoardFull(board)):
+        if not(isWinner(board, 'O')):
+            playerMove()
+            printBoard(board)
+        else:
+            print('Sorry, O\'s won this time!')
+            break
+
+        if not(isWinner(board, 'X')):
+            move = compMove()
+            if move == 0:
+                print('Tie Game!')
+            else:
+                insertLetter('O', move)
+                print('Computer placed an \'O\' in position', move , ':')
+                printBoard(board)
+        else:
+            print('X\'s won this time! Good Job!')
+            break
+
+    if isBoardFull(board):
+        print('Tie Game!')
+
+while True:
+    answer = input('Do you want to play again? (Y/N)')
+    if answer.lower() == 'y' or answer.lower == 'yes':
+        board = [' ' for x in range(10)]
+        print('-----------------------------------')
+        main()
+    else:
+        break
+
+if __name__ == "__main__":
+    main()
